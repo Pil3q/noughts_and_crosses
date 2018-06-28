3c028f3145b5cf29d960ab90b75b7d9a73f7ede8
Patryk Pilecki <ppilecki@icloud.com>
2018-06-28 20:11:43 +0100

    winning game message with tests

--- lib/game.rb
+++ lib/game.rb
@@ -14,5 +14,28 @@ class Game
     else
       raise 'hey, this field is taken'
     end
+    game_over(player)
+  end
+  def game_over(player)
+    if win?(player)
+      return "#{player.name} won"
+    elsif board.include?(" ") == false
+      return 'draw'
+    end
+  end
+
+  private
+
+  def win?(player)
+    results = winning_scenarios.map { |scenario| check(scenario, player.mark) }
+    results.include?(true)
+  end
+
+  def winning_scenarios
+    [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]], [board[0], board[3], board[6]], [board[2], board[5], board[8]], [board[1], board[4], board[7]], [board[0], board[4], board[8]], [board[2], board[4], board[6]]]
+  end
+
+  def check(arr, mark)
+    arr.all? { |move| move == mark }
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,5 +13,83 @@ describe Game do
       game.move(game.player1, 1)
       expect { game.move(game.player1, 1) }.to raise_error 'hey, this field is taken'
     end
+    it 'returns winning message right after move' do
+      expect(winning_scenario1).to eq "#{player1.name} won"
+    end
+  end
+  describe '#game_over' do
+    it 'returns a winning message for 1st scenario' do
+      winning_scenario1
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 2nd scenario' do
+      winning_scenario2
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 3rd scenario' do
+      winning_scenario3
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 4th scenario' do
+      winning_scenario4
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 5th scenario' do
+      winning_scenario5
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 6th scenario' do
+      winning_scenario6
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 7th scenario' do
+      winning_scenario7
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+    it 'returns a winning message for 8th scenario' do
+      winning_scenario8
+      expect(game.game_over(player1)).to eq "#{player1.name} won"
+    end
+
+  end
+  def winning_scenario1
+    game.move(game.player1, 1)
+    game.move(game.player1, 2)
+    game.move(game.player1, 3)
+  end
+  def winning_scenario2
+    game.move(game.player1, 4)
+    game.move(game.player1, 5)
+    game.move(game.player1, 6)
+  end
+  def winning_scenario3
+    game.move(game.player1, 7)
+    game.move(game.player1, 8)
+    game.move(game.player1, 9)
+  end
+  def winning_scenario4
+    game.move(game.player1, 1)
+    game.move(game.player1, 4)
+    game.move(game.player1, 7)
+  end
+  def winning_scenario5
+    game.move(game.player1, 2)
+    game.move(game.player1, 5)
+    game.move(game.player1, 8)
+  end
+  def winning_scenario6
+    game.move(game.player1, 3)
+    game.move(game.player1, 6)
+    game.move(game.player1, 9)
+  end
+  def winning_scenario7
+    game.move(game.player1, 1)
+    game.move(game.player1, 5)
+    game.move(game.player1, 9)
+  end
+  def winning_scenario8
+    game.move(game.player1, 3)
+    game.move(game.player1, 5)
+    game.move(game.player1, 7)
   end
 end
